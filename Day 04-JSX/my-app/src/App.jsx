48379847a5c7c0e9dc82a7da52b315f0ef09c16e
ThanhCuong <adidasgttn@gmail.com>
2023-07-06 13:32:26 +0700

    jsx

---
+++ Day 04-JSX/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h4>Browser details:{navigator.userAgent}</h4>
+    </div>
+        
+  );
+}
+
+export default App;
